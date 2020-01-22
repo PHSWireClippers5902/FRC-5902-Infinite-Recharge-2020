8d5716565cd7f5c7e52675b3633f0f7fd327f5fa
Greg Riddle <griddle2023@icloud.com>
2020-01-22 13:56:32 -0500

    Greg

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,7 +8,7 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.command.Scheduler;
+//import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
@@ -95,6 +95,8 @@ public class Robot extends TimedRobot {
           break;
         }
     }
+    
+  
   }
 
   /**
