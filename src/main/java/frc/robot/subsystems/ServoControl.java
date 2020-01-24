685372d02fde2d9c09a75497385106d635306107
SagesOfRPG <wayne@sagesofrpg.com>
2020-01-24 14:05:06 -0500

    Edits to try and get Servo Code Up

--- src/main/java/frc/robot/subsystems/ServoControl.java
+++ src/main/java/frc/robot/subsystems/ServoControl.java
@@ -7,35 +7,37 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+import frc.robot.RobotMap;
+import frc.robot.subsystems.*;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
 
-public class OI extends SubsystemBase {
-  /**
-   * Enables control through either an Xbox Controller or a Joystick
-   */
-  public static XboxController xbox = new XboxController(0);
-  public static Joystick joystick;
+
+/**
+ * Manages the movement of Lead and Following motor controllers
+ */
+public class ServoControl extends Subsystem {
+
+  OI m_oi = new OI();
+
   
-  public OI() {
-    System.out.println(xbox.toString());
+
+  
+  @Override
+  protected void initDefaultCommand() {
+    // setDefaultCommand(new ArcadeDrive());
+    
+
   }
+
+  
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
   }
 
-  public double[] getStickValues(Hand hand){
-    double x = xbox.getX(hand);
-    double y = xbox.getY(hand);
-
-    double[] returnArray = {x, y};
-    return returnArray;
+  public void stop() {
   }
 
-  public XboxController getXbox(){
-    return xbox;
-  }
 }
