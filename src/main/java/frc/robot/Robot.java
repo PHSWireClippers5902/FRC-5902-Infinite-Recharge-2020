515e00e8ccc43d3369876255509f961633a6b0d4
Casey Gladu <59513086+cgladu01@users.noreply.github.com>
2020-02-14 12:56:44 -0800

    When the lights go down in first city (#8)
    * ljjljh
    
    * Lights work correctly now

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -15,8 +15,10 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
 import frc.robot.subsystems.*;
 import frc.robot.RobotMap;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import frc.robot.subsystems.pneumaticSystem;
 import edu.wpi.first.wpilibj.Ultrasonic;
+
 /**
  * The VM is configured to automatically run this class, and to call the
  * functions corresponding to each mode, as described in the TimedRobot
@@ -30,6 +32,8 @@ public class Robot extends TimedRobot {
   private String m_autoSelected;
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
   public static DriveTrain driveTrain;
+  public static OI oi;
+  public static lightSystem lightSystem;
    public static OI oi;
   public static pneumaticSystem pneumaticSystem;
 
@@ -44,6 +48,7 @@ public class Robot extends TimedRobot {
     SmartDashboard.putData("Auto choices", m_chooser);
     RobotMap.init();
     driveTrain = new DriveTrain();
+    lightSystem = new lightSystem();
     pneumaticSystem = new pneumaticSystem();
     oi = new OI();
 
@@ -64,6 +69,7 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotPeriodic() {
+    // Smart Dashboard Numbers
     SmartDashboard.putBoolean("F Pistons", RobotMap.frontSolenoid.get());
     SmartDashboard.putBoolean("B Pistons", RobotMap.backSolenoid.get());
     SmartDashboard.putBoolean("PSI", RobotMap.compressor.getPressureSwitchValue());
@@ -84,6 +90,7 @@ public class Robot extends TimedRobot {
   public void autonomousInit() {
     m_autoSelected = m_chooser.getSelected();
     System.out.println("Auto selected: " + m_autoSelected);
+    Robot.lightSystem.getAllianceColor();
   }
 
   /**
@@ -111,8 +118,8 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopInit() {
+    Robot.lightSystem.getAllianceColor();
     RobotMap.compressor.start();
-    
   }
 
   /**
--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -2,8 +2,9 @@ package frc.robot;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
-
+import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.Ultrasonic;
@@ -13,7 +14,9 @@ public class RobotMap {
     public static WPI_VictorSPX driveFollowLeft = new WPI_VictorSPX(3);
     public static WPI_TalonSRX driveMainRight = new WPI_TalonSRX(2);
     public static WPI_VictorSPX driveFollowRight = new WPI_VictorSPX(4);
-
+    public static SpeedController lightsR;
+    public static SpeedController lightsL;
+  
     public static DifferentialDrive diffDrive = new DifferentialDrive(driveMainLeft, driveMainRight);
   // Pneumatics
   public static Compressor compressor;
@@ -30,7 +33,9 @@ public class RobotMap {
         driveMainRight.configOpenloopRamp(1);
         driveFollowLeft.configOpenloopRamp(1);
         driveFollowRight.configOpenloopRamp(1);
-       
+        //Lights
+        lightsR = new Spark(0);
+        lightsL = new Spark(3);
         // Create pneumaticSystem
         compressor = new Compressor(0);
         frontSolenoid = new SolenoidWrapper(1);
