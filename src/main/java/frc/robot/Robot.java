04e461d85c77917d2666e6b580413fb0c89ac185
Casey Gladu <59513086+cgladu01@users.noreply.github.com>
2020-02-14 16:54:46 -0500

    Revert "Merge branch 'QuickComp' into WhenTheLightsGoDownInFirstCity"
    This reverts commit 52462928c6ca961a1e5477346d4d4adf43e0d6cb, reversing
    changes made to 5e30ce90e3d087bacad24256ed5984589ceccc47.
    Revert changes from QuickComp

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -16,8 +16,7 @@ import edu.wpi.first.wpilibj.GenericHID.Hand;
 import frc.robot.subsystems.*;
 import frc.robot.RobotMap;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
-import frc.robot.subsystems.pneumaticSystem;
-import edu.wpi.first.wpilibj.Ultrasonic;
+
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -34,8 +33,8 @@ public class Robot extends TimedRobot {
   public static DriveTrain driveTrain;
   public static OI oi;
   public static lightSystem lightSystem;
-   public static OI oi;
-  public static pneumaticSystem pneumaticSystem;
+
+
 
   /**
    * This function is run when the robot is first started up and should be
@@ -49,12 +48,7 @@ public class Robot extends TimedRobot {
     RobotMap.init();
     driveTrain = new DriveTrain();
     lightSystem = new lightSystem();
-    pneumaticSystem = new pneumaticSystem();
-    oi = new OI();
 
-  }
-  @Override
-  public void disabledInit(){
   }
 
   /**
@@ -69,10 +63,9 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotPeriodic() {
+
     // Smart Dashboard Numbers
-    SmartDashboard.putBoolean("F Pistons", RobotMap.frontSolenoid.get());
-    SmartDashboard.putBoolean("B Pistons", RobotMap.backSolenoid.get());
-    SmartDashboard.putBoolean("PSI", RobotMap.compressor.getPressureSwitchValue());
+
   }
 
   /**
@@ -119,7 +112,7 @@ public class Robot extends TimedRobot {
   @Override
   public void teleopInit() {
     Robot.lightSystem.getAllianceColor();
-    RobotMap.compressor.start();
+
   }
 
   /**
@@ -129,7 +122,6 @@ public class Robot extends TimedRobot {
   public void teleopPeriodic() {
 
     driveTrain.driveWithXbox();
-    oi.buttoncheck();
     
   }
 
